ebe337d8fb539cbdaf57404f2aa44252b1b0e817
Michael Watts <wattsjmichael@gmail.com>
2020-09-25 11:44:21 -0700

    Adds failing test for Buy2get1free for Bread

--- Pierre.Tests/ModelTests/BakeryTests.cs
+++ Pierre.Tests/ModelTests/BakeryTests.cs
@@ -7,7 +7,7 @@ namespace Pierre.Test
   public class BakeryTests
   {
     [TestMethod]
-    public void isBread_NumberMultipleByTwo_True() // Sees if Bread is a interger and changeable
+    public void isBread_NumberMultipleByFive_True() // Sees if Bread is a interger and changeable
     {
       Bread testBread = new Bread();
       Assert.AreEqual(10, testBread.isBread(2));
@@ -16,15 +16,13 @@ namespace Pierre.Test
     public void isPastry_NumberMultiplyByTwo_True()// Sees if Pastry is a interger and changeable
     {
       Pastry testPastry = new Pastry();
-      Assert.AreEqual(true, testPastry.isPastry(2));
+      Assert.AreEqual(4, testPastry.isPastry(2));
     }
-    // [TestMethod]
-    // public void isBread_NumberDivideByTwoAddOneBread_Test()
-    // {
-    // //arrange
-    // Bread testBread = new Bread();
-    // int userBread = 3;
-    // Assert.AreEqual(true, testBread.isBread(userBread));
-    // }
-  }
+    [TestMethod]
+    public void isBread_BuyTwoGetOneFree_True()
+    {
+      Bread testBread = new Bread();
+      Assert.AreEqual(20, testBread.isBread(6));
+    }
+}
 }
